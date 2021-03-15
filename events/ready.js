80008f3fadabe9c05c1fc52eede01c5c8b601526
ard50500 <69609561+ard50500@users.noreply.github.com>
2021-03-15 15:33:35 +0300

    Add files via upload

---
+++ events/ready.js
@@ -0,0 +1,26 @@
+const chalk = require("chalk");
+const moment = require("moment");
+const Discord = require("discord.js");
+const ayarlar = require("../ayarlar.json");
+
+var prefix = ayarlar.prefix;
+
+module.exports = client => {
+  console.log(
+    `[${moment().format("YYYY-MM-DD HH:mm:ss")}] BOT: Aktif, Komutlar yüklendi!`
+  );
+  console.log(
+    `[${moment().format("YYYY-MM-DD HH:mm:ss")}] BOT: ${
+      client.user.username
+    } ismi ile giriş yapıldı!`
+  );
+  client.user.setStatus("online");
+  var oyun = [
+    "c",
+    "b",
+    "a"
+
+  ];
+
+
+};
